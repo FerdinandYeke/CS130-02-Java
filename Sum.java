105476f6c71c3009f4d5643767e12c2d91a5ca0d
FerdinandYeke <112411799+FerdinandYeke@users.noreply.github.com>
2022-08-29 11:52:07 -0400

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,17 @@
+//Ferdinand Yeke
+//Bellarmine University
+//CS130-2
+//Sum.java
+//This Class Prints out the sum of the numbers
+
+public class Sum{
+	public static void main(String []args){
+		int sum;
+		int number1 = 3;
+		int number2 = 5;
+		
+		sum = number1 + number2;
+		
+		System.out.println(sum);
+	}
+}
