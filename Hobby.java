5b69f27c720015a77e67fc2c6f8444b65d79a714
FerdinandYeke <112411799+FerdinandYeke@users.noreply.github.com>
2022-08-29 11:21:53 -0400

    Add files via upload

---
+++ Hobby.java
@@ -0,0 +1,19 @@
+//Hobby.java
+//This program displays my hobbies
+	
+
+public class Hobby{
+	public static void main(String []args){
+	
+	//This prints out some of my hobbies.
+	
+	System.out.println("I have many hobbies I do in my free time. They are:");
+	System.out.println("Socializing with family and friends,");
+	System.out.println("Developing my skills, like Cooking, 3D artist and learning the Software");
+	System.out.println("Exercising, Running, little bit of Football(Soccer)and Basketball");
+	System.out.println("Watching shows, movies");
+	
+	}//Main method ends here
+	
+}//Class ends here
+	
