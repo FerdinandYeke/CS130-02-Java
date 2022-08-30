6bc6c878a8e81bf5074bdd558285c435ae0650a1
FerdinandYeke <112411799+FerdinandYeke@users.noreply.github.com>
2022-08-30 19:21:48 -0400

    Add files via upload

---
+++ Sum2.java
@@ -0,0 +1,30 @@
+//Ferdinand Yeke
+//Bellarmine University
+//CS130-2
+//Sum2.java
+//This Class Prints out the sum of the numbers
+
+public class Sum2{
+	public static void main(String []args){
+		int sum;
+		int product;
+		int value;
+		int number1 = 4;
+		int number2 = 6;
+		
+		sum = number1 + number2;
+		
+		System.out.println("The Sum of number1 and number2 is: ");
+		System.out.println(sum);
+		
+		value = number1 - number2;
+		
+		System.out.println("The Subtracted value of number1 and number2 is: ");
+		System.out.println(value);
+		
+		product = number1 * number2;
+		
+		System.out.println("The product of number1 and number2 is: ");
+		System.out.println(product);
+	}
+}
